ccd47302b8ac5634ee8668014f739ad29ae67a16
Viviana  Hernandez <jeimyv.hernandezp@utadeo.edu.co>
2023-09-23 18:07:48 +0000

    comed

--- scripts/consulta.sql
+++ scripts/consulta.sql
@@ -1,7 +1,31 @@
 use adw;
 
--- Empleados
-select *
-from HumanResources_Employee
-limit 5
-;
+
+select 
+SalesOrderID,
+sales.OrderDate,
+sales.CustomerID,
+sales.SalesPersonID,
+sales.TerritoryID,
+concat(person.FirstName, ' ', person.LastName) CustomerName,
+concat(employe.FirstName, ' ', employe.LastName) SalesPersonName,
+territory.name as TerritoryName
+from Sales_SalesOrderHeader sales
+join Sales_Customer as cliente
+on sales.CustomerID= cliente.CustomerID
+left outer join Person_Person person
+on cliente.PersonId = person.BusinessEntityId
+join Sales_SalesPerson salesperson
+on sales. SalesPersonID= salesperson.BusinessEntityId
+left outer join Person_Person as employe
+on salesperson.BusinessEntityId=person.BusinessEntityId
+join Sales_SalesTerritory as territory
+on sales.TerritoryID = territory.TerritoryID
+ limit 3;
+
+ HumanResources_Department
+ HumanResources_Employee
+ HumanResources_EmployeeDepartmentHistory
+ HumanResources_EmployeePayHistory
+ Person_Person
+ Sales_SalesPerson
